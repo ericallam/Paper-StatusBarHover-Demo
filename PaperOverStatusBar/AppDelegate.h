083d9518f98065b4d39e4374c4823e2c129de23f
Eric Allam <rubymaverick@gmail.com>
2014-02-07 16:14:58 +0000

    Initial Commit

---
+++ PaperOverStatusBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PaperOverStatusBar
+//
+//  Created by Eric Allam on 07/02/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PaperOverStatusBar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PaperOverStatusBar
+//
+//  Created by Eric Allam on 07/02/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
